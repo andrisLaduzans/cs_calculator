ac3bd363219eb34362fbbe7f26aaf079d257574a
Andris Laduzans <andris.laduzans@gmail.com>
2022-09-20 22:56:32 +0300

    feat(UI): added basic UI + some core logic

---
+++ Calculator/Engine.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Engine
+{
+    public class Calc
+    {
+        private string _firstNumber;
+        public string firstNumber
+        {
+            get { return this._firstNumber; }
+        }
+
+        private string _secondNumber;
+        public string secondNumber
+        {
+            get { return this._secondNumber; }
+        }
+
+        private string _operation;
+        public string operation
+        {
+            get { return this._operation; }
+        }
+
+        private string _displayString;
+        public string displayString
+        {
+            get { return this._displayString == null ? " " : this._displayString; }
+            set { this._displayString = value; }
+        }
+
+        public Calc()
+        {
+        }
+
+        public string handleInput(string input)
+        {
+            Regex numReg = new Regex(@"^NUMBER_\d");
+            bool isNumber = numReg.IsMatch(input);
+
+            if (isNumber)
+            {
+                return this.handleNumber(input);
+            }
+            else
+            {
+                return this.handleOperator(input);
+            }
+
+        }
+
+        private string handleNumber(string input)
+        {
+            string extractedNumberStr = input.Replace("NUMBER_", "");
+            this._firstNumber += extractedNumberStr;
+            return this._firstNumber;
+        }
+
+        private string handleOperator(string operation)
+        {
+            return "not a number";
+        }
+    }
+}
+
---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Engine;
+namespace Calculator;
+
+public partial class MainPage : ContentPage
+{
+    Engine.Calc calc = new Engine.Calc();
+
+    string labelTxt = " ";
+
+    public MainPage()
+	{
+		InitializeComponent();
+		LabelTxt.Text = this.labelTxt;
+	}
+
+    private void OnClick(object sender, EventArgs e)
+    {
+        string btnEnum = (sender as Button).ClassId;
+        
+        labelTxt = this.calc.handleInput(btnEnum);
+        LabelTxt.Text = labelTxt;        
+    }
+}
